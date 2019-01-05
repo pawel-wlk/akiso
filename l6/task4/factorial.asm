de52a451af792ef4e8458f43c8437e03ec9a4cca
Paweł Wilkosz <pwilkosz5@gmail.com>
2019-01-05 01:46:29 +0100

    4th works for 64 bits

--- l6/task4/factorial.asm
+++ l6/task4/factorial.asm
@@ -1,7 +1,7 @@
 section .data
-  message: db "Welcome, please insert a number", 10, 0
+  message: db "Welcome, please insert a decimal number", 10, 0
   scanfformat: db "%d", 0
-  resultformat: db "%d! = %ld", 10, 0
+  resultformat: db "%d! = %08x%08x%08x%08x (hex)", 10, 0
 
 section .bss
   num resb 4
@@ -23,25 +23,50 @@ main:
   call scanf
   add esp, 4
 
-  movd xmm1, [num]
+  mov ecx, [num]
+  mov dword [result],1
+  movd xmm1, dword [result]
 
   calculatingloop:
     cmp ecx, 1
-    jle printing
+    je printing
 
-    ; dunno what to do
-    movd xmm1, ecx
-    pmullw xmm0, xmm1
-    
+    movd xmm2,ecx
+
+    movdqu xmm3, xmm1
+    movdqu xmm4, xmm1
+    movdqu xmm5, xmm1
+
+    psrlq xmm3, 32
+    psrlq xmm4, 64
+    psrlq xmm5, 96
+
+    pmuludq xmm1, xmm2
+    pmuludq xmm3, xmm2
+    pmuludq xmm4, xmm2
+    pmuludq xmm5, xmm2
+
+    psllq xmm3, 32
+    psllq xmm4, 64
+    psllq xmm5, 96
+
+    paddq xmm1, xmm3
+    paddq xmm1, xmm4
+    paddq xmm1, xmm5
+
+
     loop calculatingloop
-    
 
   printing:
-    push dword [result]
+    movdqu oword[result], xmm1
+    push dword [result],
+    push dword [result+4],
+    push dword [result+8],
+    push dword [result+12],
     push dword [num]
     push resultformat
     call printf
-    add esp, 10 
+    add esp, 24 
 
   mov ebx, 0
   mov eax, 1  ; sys_exit
